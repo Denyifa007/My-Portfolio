290a78216caf4117d8c076283a88035fa9d121c3
Oyeindenyifa Diegbegha <121164225+Denyifa007@users.noreply.github.com>
2023-06-02 22:08:33 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,13 +86,13 @@ const Navbar = () => {
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '>
               <a className='flex justify-between items-center w-full text-gray-300' 
-              href="/">
+              href="https://github.com/Denyifa007">
                 Github <FaGithub size={30}/>
               </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
               <a className='flex justify-between items-center w-full text-gray-300' 
-              href="/">
+              href="https://gmail.com/denyifasamuel@gmail.com">
                 Email <HiOutlineMail size={30}/>
               </a>
             </li>
